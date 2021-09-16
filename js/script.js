3ab01a57aa37cb150c7b0f16f527d6f8f9194c45
manfred-commits <manfredipiraino@gmail.com>
2021-09-16 15:42:47 +0200

    aggiunto stampa img con vue "Fine Bonus Es"

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@
 
 
 
-// 1. inizializzo una variabile vue, per poter accedere alle funzionalità di vueJs che ci permettono di accedere a una sintassi per il nostro js diversa da quella utilizzata nel clear Js in cui si utilizza il modello DOM /
+// 1. inizializzo una variabile vue, per poter accedere alle funzionalità di vueJs che ci permettono di accedere a una sintassi per il nostro js diversa da quella utilizzata nel clear Js in cui si utilizza il modello DOM.
 const application = new Vue({
 
-    // 2. creo un id, che corrisponderà al nostro "ROOT" da poter associare al html, così da poter utilizzare vueJS per quel SINGOLO elemento
+    // 2. creo un id, che corrisponderà al nostro "ROOT" da poter associare al html, così da poter utilizzare vueJS per quel SINGOLO elemento.
     el: "#application",
     data:{
-        message: "Hello there!"
+
+        // 3.Stampo a schermo un messaggio all’interno di un h1, accedendo nel'html alla keyword message.
+
+        message: "Hello there!",
+
+        // 4.Aggiungo alla pagina un’immagine, presa anch’essa da un data, collegando le proprietà di un tag img alle keyword image & imageDescription.
+
+        image: "https://vuejs.org/images/logo.png",
+        imageDescription: "vueJs logo"
     }
 
 });
